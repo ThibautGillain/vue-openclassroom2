b2b3136d098e440d9592902215aef5c0726d12ef
ThibautGillain <thibaut.gillain@insa-lyon.fr>
2022-02-08 22:23:55 +0100

    P2C2

---
+++ cafe-with-a-vue/cli-version/src/components/MenuItem.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="menu-item">
+      <img
+        class="menu-item__image"
+        :src="imgSrc"
+        :alt="imgAlt"
+      />
+      <div>
+        <h3>{{ itemName }}</h3>
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+        <div>
+            <label for="add-item-quantity"
+            >Quantity: {{ quantity }}</label
+            >
+            <input
+            v-model.number="quantity"
+            id="add-item-quantity"
+            type="number"
+            />
+            <button @click="addToShoppingCart(quantity)">
+            Add to Shopping Cart
+            </button>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: 'MenuItem',
+      props: ['imgSrc', 'imgAlt', 'itemName', 'inStock', 'quantity', 'addToShoppingCart']
+    }
+</script>
+
+<style>
+  .menu-item {
+    display: flex;
+    width: 500px;
+    justify-content: space-between;
+    margin-bottom: 30px;
+  }
+
+  .menu-item__image {
+    max-width: 300px;
+  }
+</style>
--- part-4/new-vue-cli-app/src/App.vue
+++ part-4/new-vue-cli-app/src/App.vue
@@ -1,22 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
     HelloWorld
   }
 }
 </script>
 
-<style lang="scss">
+<style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
